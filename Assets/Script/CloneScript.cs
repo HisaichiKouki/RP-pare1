28f9a915ef006d3242e65e01d6fd66e0c06652a7
ヒサイチ_コウキ <k023g0068@g.neec.ac.jp>
2024-05-29 05:05:50 +0900

    玉のサイズが最大になった時色を変える

--- Assets/Script/CloneScript.cs
+++ Assets/Script/CloneScript.cs
@@ -14,9 +14,6 @@ public class CloneScript : MonoBehaviour
     float holeSize;
     [SerializeField] bool isChange = true;
 
-    // public static HoleScript instance;
-
-    // Start is called before the first frame update
     void Start()
     {
         targetObj = GameObject.Find("Field3");
@@ -28,10 +25,7 @@ public class CloneScript : MonoBehaviour
     {
         if (isCollision)
         {
-          
-
-           
-           // Debug.Log("IsScored" + fieldScript.IsScored());
+                     
             holeSize = colSphere.transform.localScale.x;
             holeSize -= 0.02f;//拡大サイズを少し小さくしてガクガクしないようにする
             if (transform.localScale.x < holeSize)
@@ -45,48 +39,38 @@ public class CloneScript : MonoBehaviour
                 if (transform.localScale.x > holeSize)
                 {
                     transform.localScale = new Vector3(holeSize, holeSize, holeSize);
+                    //サイズがマックスになったら色を変える
+                    SetMatColor(transform.GetComponent<MeshRenderer>(), Color.yellow);
                 }
 
-                //if (fieldScript.IsScored())
-                //{
-                //    if (this.gameObject != null)
-                //    {
-                //        Destroy(this.gameObject);
-                //    }
-                //}
+                
             }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log(other.gameObject.tag);
         if (other.gameObject.tag == "CollisionSphere"|| other.gameObject.tag == "CollisionSphere_OutSide" )
         {
             isCollision = true;
             colSphere = other.gameObject;
         }
-        //if (other.gameObject.tag == "Field")
-        //{
-        //    field = other.gameObject;
-        //}
     }
 
     private void OnTriggerExit(Collider other)
     {
-        // Debug.Log(other.gameObject.tag);
         if (other.gameObject.tag == "CollisionSphere" || other.gameObject.tag == "CollisionSphere_OutSide")
         {
             isCollision = false;
         }
-        //if(other.gameObject.tag == "Hole"|| other.gameObject.tag == "OutSideHole")
-        //{
-        //    other.GetComponent<Hole2PlayerScript>().SetIsScored(false);
-        //}
     }
 
     //public void SetIsScored(bool isScored) { this.isScored = isScored; }
 
     public bool GetIsMaxScale() { return isMaxScale; }
 
+    void SetMatColor(MeshRenderer mesh, Color col)
+    {
+        mesh.material.color = col; //meshのmaterialの色を変える
+    }
 }
