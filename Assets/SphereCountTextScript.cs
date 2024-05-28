89f57c324d0ae1dcd736efdd1431e3c872117840
ヒサイチ_コウキ <k023g0068@g.neec.ac.jp>
2024-05-28 15:44:05 +0900

    玉数の描画(仮)

---
+++ Assets/SphereCountTextScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SphereCountTextScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    PlayerScript playerScript;
+    TextMeshProUGUI text;
+    void Start()
+    {
+        playerScript = FindAnyObjectByType<PlayerScript>();
+        text=GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.SetText("SpherNum="+playerScript.GetnowSphere());
+    }
+}
