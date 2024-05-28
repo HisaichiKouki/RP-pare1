07abfd460034afb5c63998c054e9f71710e462cc
ヒサイチ_コウキ <k023g0068@g.neec.ac.jp>
2024-05-29 02:11:45 +0900

    Timerを表示(仮)

---
+++ Assets/Script/GameManagerScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float MaxTime;
+    float gameTimer;
+
+    [SerializeField, Header("ÉvÉåÉCÉÑÅ[óéâ∫éûÇ…å∏ÇÁÇ∑ïbêî")] private float damegeCount;
+    public TextMeshProUGUI text;
+
+    bool isFinish;
+    void Start()
+    {
+        gameTimer = MaxTime;
+        isFinish = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!isFinish)
+        {
+            gameTimer -= Time.deltaTime;
+            if (gameTimer <=0)
+            {
+                isFinish = true;
+            }
+        }
+        text.SetText("Time" +gameTimer.ToString("f1"));
+
+    }
+
+    public void IsDamage() { gameTimer -= damegeCount; }
+}
